a11eff30ff53e915dd24180fb7c3b409532756b7
dhanu <dhanushbaskar26082004@gmail.com>
2024-12-31 14:04:18 +0530

    this is the first java program

---
+++ FORLOOP.java
@@ -0,0 +1,12 @@
+public class FORLOOP {
+    public static void main(String args[]){
+        /*for(int i=1; i<=10; i++){
+        System.out.print(i);*/
+        int i = 1;
+        while(i <= 10){
+            System.out.print(i); // also entry controlled loop 
+            i++;
+        }
+    }
+  
+}
